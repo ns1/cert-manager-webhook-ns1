4ccd03fa74cd546f5431f8c542a4be5228f7c722
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2025-05-01 13:51:59 -0400

    Bump go version; dependencies
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jetstack/cert-manager/test/acme/dns"
+	dns "github.com/cert-manager/cert-manager/test/acme"
 )
 
 var (
@@ -20,7 +20,7 @@ func TestRunsSuite(t *testing.T) {
 		dns.SetResolvedZone(zone),
 		dns.SetAllowAmbientCredentials(false),
 		dns.SetManifestPath("testdata/ns1"),
-		dns.SetBinariesPath("_out/kubebuilder/bin"),
+		// dns.SetBinariesPath("_out/kubebuilder/bin"),
 	)
 
 	fixture.RunConformance(t)
